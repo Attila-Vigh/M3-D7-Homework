68895459fc242518e3e2a394126c56ffa082c5e3
attila <attilavigh.contact@gmail.com>
2021-06-29 15:51:38 +0100

    Ex: 2) Create a dropdown (<select>) that allows the user to...
    ...select between name, username and email.        ​Create then a filter. When the user types in something, you should filter the user based on the input and on the value of the select.       ​Es.: select on NAME. Filter input = Glenna, only user id number 9 should remain

--- script.js
+++ script.js
@@ -2,22 +2,34 @@ const $_ = (selector) => document.querySelector(selector);
 const $_All = (selector) => document.querySelectorAll(selector);
 
 
+
 window.addEventListener("load", () => {
 
     getUsers()
 })
 
+const displayUserProperty = (button, userList, prop) => {
+    const displayUsers = $_("#users")
+    
+    button.addEventListener("click", ( ) =>{
+        displayUsers.innerHTML = ""
+        userList.forEach(( user ) =>
+            displayUsers.innerHTML += `<p>${ user[prop] }</p>`) 
+    })
+}
 const getUsers = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
     const userList = await response.json()
     console.log(userList);
-    let users = $_("#users")
+    // let users = 
 
-    userList.forEach(
-    (user) => {
-        console.log(user)
-        users.innerHTML += `<p>${user.name}</p>`
-    })
+    const buttonName = $_("#btn-name")
+    const buttonUserName = $_("#btn-user-name")
+    const buttonEmail = $_("#btn-email")
 
+    displayUserProperty(buttonName, userList, "name")
+    displayUserProperty(buttonUserName, userList, "username")
+    displayUserProperty(buttonEmail, userList, "email")
 
 }
+
